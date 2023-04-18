e3e5d44490e9aff05c05beb4d525826da95d9ed1
Ramon Calix <ramoncalix@Ramons-MacBook-Pro.local>
2023-04-18 11:50:46 -0600

    testing libraries updated

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
     roots: ["<rootDir>/src"],
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: [
+      'src/**/*.{js,tsx}',
+      '!src/**/*.svg'
+    ],
     testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
     transform: {
       "^.+\\.(ts|tsx)$": "ts-jest",
+      "^.+\\.svg$": "<rootDir>/svgTransform.js"
     },
     moduleNameMapper: {
       "\\.(css|less|scss|sass)$": "identity-obj-proxy",
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { useState } from 'react'
-import './App.css'
-
+import Home from './pages/Home'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="card w-96 bg-primary text-primary-content">
-    <div className="card-body">
-      <h2 className="card-title">Card title!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <p data-testid="count" className="count">Count: {count}</p>
-      <div className="card-actions justify-center">
-        <button className="btn" onClick={() => setCount(count + 1)}>Buy Now</button>
-      </div>
+    <div>
+      <Home/>
     </div>
-  </div>
+
   )
 }
 
